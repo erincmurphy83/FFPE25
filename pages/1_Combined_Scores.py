8c10db05e79a64b41de36ff210e81cb792e5e77f
Erin M <35467342+erincmurphy83@users.noreply.github.com>
2025-01-12 15:32:06 -0500

    Update 1_Combined_Scores.py

--- pages/1_Combined_Scores.py
+++ pages/1_Combined_Scores.py
@@ -3,12 +3,9 @@ import pandas as pd
 
 st.header("**Scoreboard**", divider='gray')
 
-
 if "df" not in st.session_state:
     st.session_state.df = pd.read_csv("data/managers_games.csv", index_col=[0])
 
-# df = pd.read_csv("Playoff_Football_Dashboard/managers_games.csv", index_col=[0])
-
-df1 = st.session_state.df.sort_values("Manager")  #.sum()
+df1 = st.session_state.df.groupby('Manager').size().reset_index(name='Players Remaining')
 
 st.dataframe(df1, hide_index=True, height=500)
