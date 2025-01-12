da5a81c5ef38a6dd099cacdd64e0388702528f14
Erin M <35467342+erincmurphy83@users.noreply.github.com>
2025-01-12 15:07:54 -0500

    Update 2_Individual_Teams.py

--- pages/2_Individual_Teams.py
+++ pages/2_Individual_Teams.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.header("**Individual Team**", divider='gray')
 
 if "df" not in st.session_state:
-    st.session_state.df = pd.read_csv("Playoff_Football_Dashboard/data/managers_games.csv", index_col=[0])
+    st.session_state.df = pd.read_csv("data/managers_games.csv", index_col=[0])
 
 
 col1, col2 = st.columns(2)
