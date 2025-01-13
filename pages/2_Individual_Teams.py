afcfccc5dfdc92e9236deb52c53d672740fb7950
Erin M <35467342+erincmurphy83@users.noreply.github.com>
2025-01-12 20:21:59 -0500

    Update 2_Individual_Teams.py

--- pages/2_Individual_Teams.py
+++ pages/2_Individual_Teams.py
@@ -4,19 +4,21 @@ import streamlit as st
 st.header("**Individual Team**", divider='gray')
 
 if "df" not in st.session_state:
-    st.session_state.df = pd.read_csv("data/managers_games.csv", index_col=[0])
-
+    st.session_state.df = pd.read_csv("data/total_points.csv", index_col=[0])
 
 col1, col2 = st.columns(2)
 with col1:
-    option = st.selectbox("Who's team do you wish to see",
-                          sorted(st.session_state.df['Manager'].unique()))
+    st.text("Select a Manager: ")   
+
+    option = st.selectbox('',
+                          sorted(st.session_state.df['Manager'].unique()) )
 with col2:
     st.text('Total Points: ')
-    # st.write(st.session_state.df[st.session_state.df["Manager"] == option].sum()["Points"])
+    df2 = st.session_state.df.groupby(['Manager']).sum('Total Points').reset_index() 
+    st.write(df2[df2["Manager"] == option]["Total Points"].values[0])
 
 st.divider()
 
 df1 = st.session_state.df[st.session_state.df["Manager"] == option].loc[:, st.session_state.df.columns != 'Manager']
 
-st.dataframe(df1, hide_index=True, height=500)
+st.dataframe(df1, hide_index=True, height=500, use_container_width=True)
