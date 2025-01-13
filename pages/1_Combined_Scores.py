5feea70857ad27aa4fad3a6c3ab98248b8d8a7d5
Erin M <35467342+erincmurphy83@users.noreply.github.com>
2025-01-12 20:20:30 -0500

    Update 1_Combined_Scores.py

--- pages/1_Combined_Scores.py
+++ pages/1_Combined_Scores.py
@@ -3,9 +3,19 @@ import pandas as pd
 
 st.header("**Scoreboard**", divider='gray')
 
+
 if "df" not in st.session_state:
     st.session_state.df = pd.read_csv("data/managers_games.csv", index_col=[0])
 
+if "total_points_df" not in st.session_state:
+    st.session_state.total_points_df = pd.read_csv("data/total_points.csv", index_col=[0])
+
+
 df1 = st.session_state.df.groupby('Manager').size().reset_index(name='Players Remaining')
 
-st.dataframe(df1, hide_index=True, height=500)
+# df1 = df.groupby('Manager')["Division"].sum().reset_index(name='Players Remaining')
+
+df2 = st.session_state.total_points_df.groupby(['Manager']).sum('Total_points')  #.reset_index()
+df2 = df2.merge(df1, on='Manager').sort_values(by='Total Points', ascending=False) 
+
+st.dataframe(df2, hide_index=True, height=750, use_container_width=True)
