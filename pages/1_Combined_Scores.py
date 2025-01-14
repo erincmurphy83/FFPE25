1f000dc5dc02175cbccd40c0bdea80a9b3dd0e2d
Erin M <35467342+erincmurphy83@users.noreply.github.com>
2025-01-13 21:58:02 -0500

    Update 1_Combined_Scores.py
    Minor fixes

--- pages/1_Combined_Scores.py
+++ pages/1_Combined_Scores.py
@@ -3,16 +3,11 @@ import pandas as pd
 
 st.header("**Scoreboard**", divider='gray')
 
-if "df" not in st.session_state:
-    st.session_state.df = pd.read_csv("data/managers_games.csv", index_col=[0])
+df1 = pd.read_csv("data/managers_games.csv", index_col=[0])   
+total_points_df = pd.read_csv("data/total_points.csv", index_col=[0])
 
-if "total_points_df" not in st.session_state:
-    st.session_state.total_points_df = pd.read_csv("data/total_points.csv", index_col=[0])
-
-
-df1 = st.session_statedf.groupby('Manager')["Division"].sum().reset_index(name='Players Remaining')
-
-df2 = st.session_state.total_points_df.groupby(['Manager']).sum('Total Points')  #.reset_index()
+df1 = df1.groupby('Manager')["Division"].sum().reset_index(name='Players Remaining')
+df2 = total_points_df.groupby(['Manager']).sum('Total Points')  #.reset_index()
 df2 = df2.merge(df1, on='Manager').sort_values(by='Total Points', ascending=False) 
 
 # col_order = ['Manager', 'Players Remaining', 'Total Points', 'Total_WC', 'Total_Div', 'Total_Conf', 'Total_SB']
