881bbbd78692e2179aa345a7f46d6400b705565a
Erin M <35467342+erincmurphy83@users.noreply.github.com>
2025-01-13 21:42:58 -0500

    Update Overview.py

--- Overview.py
+++ Overview.py
@@ -2,12 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="Fantasy Football Playoff Edition", page_icon="🏈", layout="wide", initial_sidebar_state="expanded")
 st.header("**Fantasy Football Playoff Edition**", divider='gray')
-# st.sidebar.markdown("# Team Selection")
 
 images = ["images/FFPE_payouts.png", "images/points_25.png"]
 captions = ["Prizes", "Scoring system"]
-
-# st.image(images, caption=captions, use_column_width=True)
 
 col1, col2 = st.columns(2)
 
@@ -16,8 +13,5 @@ with col1:
 
 with col2:
     st.image(images[1], captions[1])
-
-
-
 
 # streamlit run Playoff_Football_Dashboard/Overview.py
