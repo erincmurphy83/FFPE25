fd3c7a702d7842af961e89a8f6fe97068d8485e2
Erin M <35467342+erincmurphy83@users.noreply.github.com>
2025-01-14 21:05:42 -0500

    Update 2_Individual_Teams.py
    Updating column names

--- pages/2_Individual_Teams.py
+++ pages/2_Individual_Teams.py
@@ -3,15 +3,11 @@ import streamlit as st
 
 st.header("**Individual Team**", divider='gray')
 
-# if "df" not in st.session_state:
-#     st.session_state.df = pd.read_csv("data/total_points.csv", index_col=[0])
-
 df = pd.read_csv("data/total_points.csv", index_col=[0])
 
 col1, col2 = st.columns(2)
 with col1:
-    st.text("Select a Manager: ")   
-
+    st.text("Select a Manager: ") 
     option = st.selectbox('Select a Manager',
                           sorted(df['Manager'].unique()),
                           label_visibility="collapsed")
@@ -27,7 +23,7 @@ st.divider()
 
 df1 = df[df["Manager"] == option].loc[:, df.columns != 'Manager']
 
-col_order = ['Team', 'Player', 'Total Points', 'Total_WC', 'Total_Div', 'Total_Conf', 'Total_SB']
+col_order = ['Team', 'Player', 'Total Points', 'Total Wildcard', 'Total Division', 'Total Conference', 'Total Superbowl']
 df1 = df1[col_order]
 
 st.dataframe(df1, hide_index=True, height=550, use_container_width=True)
