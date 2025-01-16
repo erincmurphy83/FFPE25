8d7dad2688ef745d1e763638355460abb267a521
Erin M <35467342+erincmurphy83@users.noreply.github.com>
2025-01-15 20:09:29 -0500

    Update 1_Combined_Scores.py
    Rounding values

--- pages/1_Combined_Scores.py
+++ pages/1_Combined_Scores.py
@@ -7,7 +7,7 @@ df1 = pd.read_csv("data/managers_games.csv", index_col=[0])
 total_points_df = pd.read_csv("data/total_points.csv", index_col=[0])
 
 df1 = df1.groupby('Manager')["Division"].sum().reset_index(name='Players Remaining')
-df2 = total_points_df.groupby(['Manager']).sum('Total Points')  #.reset_index()
+df2 = total_points_df.groupby(['Manager']).sum('Total Points').round(2)  #.reset_index()
 df2 = df2.merge(df1, on='Manager').sort_values(by='Total Points', ascending=False) 
 
 col_order = ['Manager', 'Players Remaining', 'Total Points', 'Total Wildcard', 'Total Division', 'Total Conference', 'Total Superbowl']
