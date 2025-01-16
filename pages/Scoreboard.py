5fc26f7a1e5d54e51dce66f8757a7d54400ec1ab
Erin M <35467342+erincmurphy83@users.noreply.github.com>
2025-01-15 20:33:18 -0500

    Update Scoreboard.py

--- pages/Scoreboard.py
+++ pages/Scoreboard.py
@@ -1,29 +1,16 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-st.header("**Individual Team**", divider='gray')
+st.header("**Scoreboard**", divider='gray')
 
-df = pd.read_csv("data/total_points.csv", index_col=[0])
+df1 = pd.read_csv("data/managers_games.csv", index_col=[0])   
+total_points_df = pd.read_csv("data/total_points.csv", index_col=[0])
 
-col1, col2 = st.columns(2)
-with col1:
-    st.text("Select a Manager: ") 
-    option = st.selectbox('Select a Manager',
-                          sorted(df['Manager'].unique()),
-                          label_visibility="collapsed")
-    
-with col2:
-    st.text('Total Points: ')
-    df2 = df.groupby(['Manager']).sum('Total Points').reset_index() 
-    st.metric("Total Points", 
-              round(df2[df2["Manager"] == option]["Total Points"].values[0], 2),
-              label_visibility="collapsed")
+df1 = df1.groupby('Manager')["Division"].sum().reset_index(name='Players Remaining')
+df2 = total_points_df.groupby(['Manager']).sum('Total Points').round(2)  #.reset_index()
+df2 = df2.merge(df1, on='Manager').sort_values(by='Total Points', ascending=False) 
 
-st.divider()
+col_order = ['Manager', 'Players Remaining', 'Total Points', 'Total Wildcard', 'Total Division', 'Total Conference', 'Total Superbowl']
+df2 = df2[col_order]
 
-df1 = df[df["Manager"] == option].loc[:, df.columns != 'Manager']
-
-col_order = ['Team', 'Player', 'Total Points', 'Total Wildcard', 'Total Division', 'Total Conference', 'Total Superbowl']
-df1 = df1[col_order]
-
-st.dataframe(df1, hide_index=True, height=550, use_container_width=True)
+st.dataframe(df2, hide_index=True, height=750, use_container_width=True)
