d06ba73c43a7bc2c068f1238899bff2fc9bef4f6
Erin M <35467342+erincmurphy83@users.noreply.github.com>
2025-01-22 22:46:51 -0500

    Update Scoreboard.py
    Adding a rank column

--- pages/Scoreboard.py
+++ pages/Scoreboard.py
@@ -10,7 +10,11 @@ df1 = df1.groupby('Manager')["Conference"].sum().reset_index(name='Players Remai
 df2 = total_points_df.groupby(['Manager']).sum('Total Points').round(2)  #.reset_index()
 df2 = df2.merge(df1, on='Manager').sort_values(by='Total Points', ascending=False) 
 
-col_order = ['Manager', 'Players Remaining', 'Total Points', 'Total Wildcard', 'Total Division', 'Total Conference', 'Total Superbowl']
+# Add a ranking column based on 'Total Points'
+df2['Rank'] = df2['Total Points'].rank(method='max', ascending=False).astype(int)
+
+# Reorder the columns
+col_order = ['Rank', 'Manager', 'Players Remaining', 'Total Points', 'Total Wildcard', 'Total Division', 'Total Conference', 'Total Superbowl']
 df2 = df2[col_order]
 
 st.dataframe(df2, hide_index=True, height=750, use_container_width=True)
