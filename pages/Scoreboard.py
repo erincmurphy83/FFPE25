85a61ffccc12cb42026f4b2c299215e1dedab451
Erin M <35467342+erincmurphy83@users.noreply.github.com>
2025-02-10 22:21:39 -0500

    Update Scoreboard.py
    Final updates

--- pages/Scoreboard.py
+++ pages/Scoreboard.py
@@ -6,7 +6,7 @@ st.header("**Scoreboard**", divider='gray')
 df1 = pd.read_csv("data/managers_games.csv", index_col=[0])   
 total_points_df = pd.read_csv("data/total_points.csv", index_col=[0])
 
-df1 = df1.groupby('Manager')["Super Bowl"].sum().reset_index(name='Players Remaining')
+df1 = df1.groupby('Manager')["Winner"].sum().reset_index(name='Players Remaining')
 df2 = total_points_df.groupby(['Manager']).sum('Total Points').round(2)  #.reset_index()
 df2 = df2.merge(df1, on='Manager').sort_values(by='Total Points', ascending=False) 
 
